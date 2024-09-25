0b63cb26e9238789840fb8c37e0599be997f2016
Tiff0321 <2325287709@qq.com>
2024-09-25 17:00:36 +0800

    安装mybatis生成器

---
+++ src/main/java/com/cod/crm/controllers/UserController.java
@@ -0,0 +1,19 @@
+
+
+
+package com.cod.crm.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	@RequestMapping(value="/api/user/create",method=RequestMethod.GET)
+	public String create() {
+		
+		
+	}
+
+}
